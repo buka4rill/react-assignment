4d4033dc946f0562f78741a6cbdbf0f7dfb13d41
WEB DEVELOPER <buka4rill@gmail.com>
2020-08-18 04:53:32 +0100

    Renamed Navbar to Header, added Navs

--- react-assignment/src/App.js
+++ react-assignment/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/layout/Header';
 import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Navbar />
     </div>
   );
--- react-assignment/src/components/layout/Navbar.js
+++ react-assignment/src/components/layout/Navbar.js
@@ -1,86 +1,29 @@
 import React from 'react'
-import logo from './fcmb.png';
-import vector from './Vector.png';
-import profileImg from './profileImg.png';
-import caret from './Vector2.png';
 
-import PropTypes from 'prop-types';
-
-function Navbar({ title }) {
+const Navbar = () => {
     return (
-        <div className="container-fluid bg-white">
-            <nav className="navbar navbar-expand-lg bg-white">
-                {/* Menu Bars */}
-                <div className="navbar-nav menu-bars">
-                    <span id="top"></span>
-                    <span id="mid"></span>
-                    <span id="bottom"></span>
-                </div>
-
-                {/* Logo Section */}
-                <div className="navbar-brand logoSection ml-5">
-                    <img src={logo} alt="logo"/>
-                    &nbsp;&nbsp;
-                    <span> { title } </span>
-                    &nbsp;&nbsp;
-                    <span> ... </span>
-                </div>
-
-                {/* Search Input Section */}
-                <div className="navbar-nav ml-5">
-                    <form action="">
-                        <div className="form-group d-flex align-items-center justify-items-center">
-                            <label htmlFor="" className="d-flex mx-auto my-auto align-items-center justify-items-center">
-                                <i class="fas fa-search" style={{ color: 'rgb(224 223 223)' }}></i>
-                            </label>
-                            &nbsp;&nbsp;&nbsp;
-                            <input type="text" className="form-control" style={{ border: 'none', fontFamily: 'Nunito Sans' }} placeholder="Search" />
-                        </div>
-                    </form>
-                </div>
-
-                {/* User Section */}
-                <div className="navbar-nav ml-auto">
-                    <div className="navbar-item my-auto">
-                        <i class="far fa-bell"></i>
-                    </div>
-
-                    <div className="navbar-item my-auto ml-5">
-                        <div className="owner-tag">
-                            owner
-                        </div>
-                    </div>
-
-                    <div className="navbar-item fancy-tag my-auto ml-5">
-                        <img src={vector} alt="vector" />
-                    </div>
-
-                    <div className="navbar-item profile d-flex  ml-5">
-                        <div className="profile-img my-auto">
-                            <img src={profileImg} alt="prof_img"/>
-                        </div>
-
-                        <span className="profile-name ml-3">
-                            Chioma Davis <br />
-                            <small className="profile-email" style={{ color: '#23B3E8' }}>chioma@natabase.com</small>
-                        </span>
-
-                        <div>
-                            <img src={caret} alt="dd"/>
-                        </div>
-                        
-                    </div>
-                </div>
-            </nav>
+        <div className="navbar navbar-expand-lg py-3">
+            <div className="container nav-container px-0 py-2">
+                <ul className="navbar-nav mr-auto links">
+                    <li className="nav-item mr-4">
+                        <a href="!#" className="nav-link"> Overview </a>
+                    </li>
+                    <li className="nav-item mr-4">
+                        <a href="!#" className="nav-link"> Teams </a>
+                    </li>
+                    <li className="nav-item mr-4">
+                        <a href="!#" className="nav-link"> Modules </a>
+                    </li>
+                    <li className="nav-item mr-4">
+                        <a href="!#" className="nav-link"> Files </a>
+                    </li>
+                    <li className="nav-item"> 
+                        <a href="!#" className="nav-link"> Progress </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
 
-Navbar.defaultProps = {
-    title: 'FCMB: Mobile Banking',
-};
-  
-Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-};
 export default Navbar;
